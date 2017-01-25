52f8b9a6eed12fbdde1d529bad1ff2c31b0ee080
Bill Seremetis <bill@seremetis.net>
2017-01-25 12:44:06 +0200

    Update for D8 with composer.

--- DockerDrupalDev.sh
+++ DockerDrupalDev.sh
@@ -2,7 +2,7 @@
 
 docker-compose up -d
 
-if [ ! -f sites/default/settings.php ]; then
+if [ ! -f web/sites/default/settings.php ]; then
   cat <<EOF > sites/default/settings.php
 <?php
 \$update_free_access = FALSE;
@@ -31,7 +31,7 @@ EOF
   echo "\$drupal_hash_salt = '$salt';" >> sites/default/settings.php
 
   # Create required filesystem folders
-  mkdir -p sites/default/files && chmod 777 sites/default/files
+  mkdir -p web/sites/default/files && chmod 777 web/sites/default/files
   # mkdir -p temp && chmod 777 temp
 
   echo "Drupal initialized."
@@ -39,17 +39,19 @@ fi
 
 # Enable stage_file_proxy module to work locally with assets from a remote site
 if [ $1 ]; then
-  docker-compose exec --user 82 php drush dl stage_file_proxy -y
-  docker-compose exec --user 82 php drush en stage_file_proxy -y
-  docker-compose exec --user 82 php drush vset stage_file_proxy_origin $1
-  docker-compose exec --user 82 php drush cc all
+  docker-compose exec php drush --root=/var/www/html/web dl stage_file_proxy -y
+  docker-compose exec php drush --root=/var/www/html/web en stage_file_proxy -y
+  docker-compose exec php drush --root=/var/www/html/web vset stage_file_proxy_origin $1
+  docker-compose exec php drush --root=/var/www/html/web cr
   echo "Stage file proxy installed."
 fi
 
-alias drush="docker-compose exec --user 82 php drush"
+alias drush="docker-compose exec php drush --root=/var/www/html/web"
 alias stopdev="docker-compose kill && unalias drush && unalias stopdev && echo 'Bye bye!'"
 echo "Drush configured for dockerized drush."
 echo "Type 'stopdev' to shut down docker and unset all aliases."
 echo "Docker up and running, start working."
+echo "Taking you to Drupal root folder."
+cd web
 echo "Use the link below to login as user 1:"
-docker-compose exec --user 82 php drush uli
+docker-compose exec php drush --root=/var/www/html/web --uri=http://localhost:8000 uli
